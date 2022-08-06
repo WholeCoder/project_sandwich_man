42e2bb2fd6d09cba720228db3444f10a2c5fbfa5
RP <rpierich@hotmail.com>
2022-08-06 02:07:28 -0400

    Turned string compression representation into byte array.

--- bit_library.go
+++ bit_library.go
@@ -9,20 +9,17 @@ func InitNewByteset(bray []byte) BitsetByte {
 func (b BitsetByte) GetBit(index int) bool {
 	pos := index / 8
 	j := uint(7 - index%8)
-	fmt.Printf("\nbit at element (GetBit):  %v", b[pos])
 	return (b[pos] & (byte(1) << j)) != 0
 }
 
 func (b BitsetByte) SetBit(index int, value bool) {
 	pos := index / 8
 	j := uint(7 - index%8)
-	fmt.Println("j =", j)
 	if value {
 		b[pos] |= (byte(1) << j)
 	} else {
 		b[pos] &= ^(byte(1) << j)
 	}
-	fmt.Printf("\nBitset element (in SetBit):  %8b\n", b[pos])
 }
 
 func (b BitsetByte) Len() int {
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "encoding/binary"
     "strings"
 	"bufio"
+    "math"
 	"fmt"
 	"log"
 	"os"
@@ -23,13 +25,43 @@ func main() {
     // build hash used to encode letters to binary sequences
     printEncodingHash(encodingHash)
 
-    originalText := "The"
+    originalText := "Thequickbrownfoxjumpedoverthelazydog"
     compressedText := compressText(&encodingHash, originalText)
 
     fmt.Println("originalText:  ", originalText)
     fmt.Println("compressed  :  ", compressedText)
-//    node := hashForEncoding["z"]
 
+    lengthOfCompressedText := len(compressedText)
+    byteLengthOfCompressedText := uint64(math.Ceil(float64(lengthOfCompressedText) / 8.0)+ 8.0)// add 8.0 bytes for this size (byteLengthOfCompressedText
+
+    fmt.Println("lengthOfCompressedText: ", lengthOfCompressedText)
+    fmt.Println("byteLengthOfCompressedText: ", byteLengthOfCompressedText)
+
+    bray := make([]byte, byteLengthOfCompressedText)
+
+    fmt.Println(byteLengthOfCompressedText)
+
+    b := make([]byte, 8)
+    binary.BigEndian.PutUint64(b, uint64(byteLengthOfCompressedText-8))
+
+    count := 0
+    for count < 8 {
+        bray[count] = b[count]
+        count++
+    }
+
+    compressedTextAsByteRay := InitNewByteset(bray)
+    fmt.Println("bray = ", bray)
+
+
+    for index, number := range compressedText {
+        compressedTextAsByteRay.SetBit(index + 64, string(number) == "1")
+    }
+
+    fmt.Println("compressedTextAsByteRay = ", compressedTextAsByteRay)
+//    node := hashForEncoding["z"]
+    i :=  uint64(binary.BigEndian.Uint64(compressedTextAsByteRay[0:8]))
+    fmt.Println("length of data in bray = ", i)
 }
 
 func compressText(encodingHash *map[string]string, originalText string) string {
